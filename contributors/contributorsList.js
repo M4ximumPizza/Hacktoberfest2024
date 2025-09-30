96b82e0a7bcbbbae1d4aee7bed75a0c08df707e9
M4ximumPizza <109777745+M4ximumPizza@users.noreply.github.com>
2025-09-29 22:06:42 -0700

    Added M4ximumPizza

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2657,4 +2657,10 @@ contributors = [
     username: "https://github.com/Incharajayaram",
   },
 
+  {
+    id: 471,
+    fullname: "M4ximumPizza",
+    username: "https://github.com/M4ximumPizza",
+  }
+
 ];
